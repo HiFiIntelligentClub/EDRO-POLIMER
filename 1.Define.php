db59ba698d9b24062118b06fcb231e08d04605f3
Andrey Chekmarev <assminog@gmail.com>
2021-05-25 13:55:22 +0300

    Add files via upload

---
+++ 1.Define.php
@@ -0,0 +1,49 @@
+<?php
+///Сервер сеть
+define('сКлючь'								, '4aPrIsAForaPr'						);
+
+
+define('strProto'							, 'tcp'								);
+
+define('strDomain'							, '192.168.1.198'						);
+//define('strDomain'							, '127.0.0.1'							);
+
+define('strPort'							, '80'								);
+//define('strPort'							, '8080'								);
+
+//Загрузчик
+
+define('сРасположениеБазовойСистемыЗагрузчик'				, '/home/EDRO.SetOfTools/1.Система/2.Loader/0.loader.php'	);
+
+define('сРасположениеБазовойСистемыЗагрузчикEDRO'			, '/home/EDRO.SetOfTools/1.Система/2.Loader/0.loader.php'	);
+
+define('сРасположениеРесурсы'						, '/home/EDRO.SetOfTools/2.Ресурсы'				);
+
+//КИМ
+
+define('сРасположениеБазовойСистемыКириллИМефодий'			, '/home/EDRO.SetOfTools/1.Система/1.КиМ/0.КиМ.php'		);
+
+//define('сРасположениеБазовойСистемыОповещенияАдминистратора'		, '/home/EDRO.SetOfTools/1.Система/0.ErrorReporter/0.Report.php');
+
+
+//Журналы
+
+define('сШагЗаписьВЖурнал'						, '/home/EDRO.SetOfTools/4.Reports/Operations_report.txt'	);
+
+define('сШагЗаписьВЖурналОшибок'					, '/home/EDRO.SetOfTools/4.Reports/Error_report.txt'		);
+
+
+//Журналы//Счётчики вход
+
+define('сРасположениеБазовойСистемыСчётчикВход'				, '/home/EDRO.SetOfTools/3.Reports/0.CountUp/Вход.plmr'		);
+
+define('сРасположениеБазовойСистемыСчётчикВходИстор'			, '/home/EDRO.SetOfTools/3.Reports/1.CountUpHistory/Вход.plmr'	);
+
+//БазаДанных
+
+define('strDataBase'							, '/home/ЕДРО:ПОЛИМЕР/о2о.БазаДанных/HiFiIntelligentClub'	);
+
+
+
+
+?>
